4a9d49e6d55f09ba6541d221a13334266d842e1e
Matias Glessi <matiasglessi@gmail.com>
2016-07-26 17:11:56 -0300

    Remove method from dao

--- project/dao/user_dao.py
+++ project/dao/user_dao.py
@@ -1,29 +1,6 @@
 from project.db_connection import SqlConnection
 
-from project.entities import User
-from project.exceptions import EntityNotFound, BDError
-
 connector = SqlConnection()
-
-
-def savePlayerDao(player):
-    cursor = connector.connect()
-    cursor = connector.cursor()
-
-    cursor = cnx.cursor()
-
-    add_person = ("INSERT INTO player "
-                  "(username, email, password) "
-                  "VALUES (%s, %s, %s)")
-
-    data_player = (player.username, player.email, player.password)
-
-    cursor.execute(add_person, data_player)
-
-    # Make sure data is committed to the database
-    cnx.commit()
-
-    cursor.close()
 
 
 def save_user(user):
